909307c9b0f3c69240f19d46db14abddff9de7b0
AndrewPeng-tcssd <cindy.zhang920@gmail.com>
2025-05-31 10:58:34 -0700

    added stuff

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ give_start = 4
 computer_card = ["Keyboard", "Mouse", "Monitor", "FireFox", "CPU", "GPU", "Google", "Gaming PC", "Supercomputer", "Quantum Computer"]
 
 computer_card_stats = [
-                        ["Click clack", 50, 10, 5, "Normal"], 
-                        ["Click.", 50, 7, 5, "Normal"], 
-                        ["Shows stuff.", 80, 3, 2, "Normal"], 
-                        ["Browse.", 35, 10, 7, "Uncommon"], 
-                        ["Finishes tasks quickly but vulnerable to heat.", 25, 15, 5, "Uncommon"], 
-                        ["Great for taking down groups.", 30, 5, 10, "Rare"], 
-                        ["Better than FireFox.", 50, 10, 5, "Rare"], 
-                        ["Made for gaming", 70, 15, 10, "Epic"], 
-                        ["Superfast!", 60, 10, 20, "Epic"],
-                        ["Quantum physics!", 60, 17, 15, "Legendary"],
+                        ["Click clack", 25, 10, 5, "Normal"], 
+                        ["Click.", 25, 7, 5, "Normal"], 
+                        ["Shows stuff.", 40, 3, 2, "Normal"], 
+                        ["Browse.", 17.5, 10, 7, "Uncommon"], 
+                        ["Finishes tasks quickly but vulnerable to heat.", 12.5, 15, 5, "Uncommon"], 
+                        ["Great for taking down groups.", 15, 5, 10, "Rare"], 
+                        ["Better than FireFox.", 25, 10, 5, "Rare"], 
+                        ["Made for gaming", 35, 15, 10, "Epic"], 
+                        ["Superfast!", 30, 10, 20, "Epic"],
+                        ["Quantum physics!", 30, 17, 15, "Legendary"],
                     ]
 
 
@@ -36,7 +36,6 @@ computer_card_stats = [
 pack_names = ["Computer Pack"]
 
 all_packs = [computer_card]
-
 
 def sleep(sleep_time):
     time.sleep(sleep_time)
@@ -100,14 +99,14 @@ while True:
     if done_intro == False:
         intro_1 = input("Welcome to Card Battle! Find a friend to play with you. Press on 'c' to continue. ")
         if intro_1 == "c":
-            sleep(0.3)
+            sleep(0)
             intro_2 = input("Now decide who will be player 1 and who will be player 2. Press on 'c' to continue. ")
             if intro_2 == "c":
-                sleep(0.3)
+                sleep(0)
                 intro_3 = input("Player 1 and Player 2, you will both receive 4 random cards. Press 'c' to continue. ")
                 roll_start()
                 if intro_3 == "c":
-                    sleep(1)
+                    sleep(0)
                     print(f"Player 1 received: {', '.join(p1_cards)}")
                     print(f"Player 2 received: {', '.join(p2_cards)}")
                     intro_3_1 = input("Press c to continue: ")
@@ -125,112 +124,74 @@ while True:
             print(f"Player 2 has: {', '.join(p2_cards)}") 
             m1_1 = input("Press c to continue: ")
             if m1_1 == "c":
-                m1_1_r1 = random.randint(1,2)
-                print("Rolling.")
-                sleep(0.2)
-                print("Rolling..")
-                sleep(0.2)
-                print("Rolling...")
-                if m1_1_r1 == 1:
-                    print("Player 1 goes first!")
-                    going = 1
-                    while True:
-                        print(f"Player 1 has: {', '.join(p1_cards)}")
-                        print(f"Player 2 has: {', '.join(p2_cards)}") 
-                        m1_1_r1_p1_c1 = input("Player 1 card: ")
-                        if m1_1_r1_p1_c1 in p1_cards:
-                            m1_1_r1_p2_c1 = input("Player 2 card: ")
-                            if m1_1_r1_p2_c1 in p2_cards:
-                                print(f"Player 1 chose {m1_1_r1_p1_c1}! Player 2 chose {m1_1_r1_p2_c1}!")
-                                if m1_1_r1_p1_c1 in computer_card:
-                                    c1_stats = computer_card_stats[computer_card.index(m1_1_r1_p1_c1)]
-                                    c1_hp = c1_stats[1]
-                                    c1_atk = c1_stats[2]
-                                    c1_def = c1_stats[3]
-                                if m1_1_r1_p2_c1 in computer_card:
-                                    c2_stats = computer_card_stats[computer_card.index(m1_1_r1_p2_c1)]
-                                    c2_hp = c2_stats[1]
-                                    c2_atk = c2_stats[2]
-                                    c2_def = c2_stats[3]
-                                if going == 1:
-                                    c2_hpn, c2_hp, c2_defed = battle(c1_hp, c1_atk, c1_def, c2_hp, c2_atk, c2_def, 1)
-                                    print(f"Player 1's {m1_1_r1_p1_c1} goes first! It does {(c2_hp - c2_hpn) + c2_defed} damage {m1_1_r1_p2_c1} blocks {c2_defed} damage! {m1_1_r1_p2_c1} now has {c2_hpn} HP!")
-                                    c2_hp = c2_hpn
-                                    if c2_hp <= 0:
+                going = 1
+                print(f"Player 1 has: {', '.join(p1_cards)}")
+                print(f"Player 2 has: {', '.join(p2_cards)}")
+                temp1 = [{p1_cards[0]: computer_card_stats[computer_card.index(p1_cards[0])][1]}, 
+                            {p1_cards[1]: computer_card_stats[computer_card.index(p1_cards[1])][1]},
+                            {p1_cards[2]: computer_card_stats[computer_card.index(p1_cards[2])][1]},
+                            {p1_cards[3]: computer_card_stats[computer_card.index(p1_cards[3])][1]}]
+                temp2 = [{p2_cards[0]: computer_card_stats[computer_card.index(p2_cards[0])][1]},
+                            {p2_cards[1]: computer_card_stats[computer_card.index(p2_cards[1])][1]},
+                            {p2_cards[2]: computer_card_stats[computer_card.index(p2_cards[2])][1]},
+                            {p2_cards[3]: computer_card_stats[computer_card.index(p2_cards[3])][1]}]
+                while True:
+                    c1_w = False
+                    c2_w = False
+                    m1_p1 = input("Player 1 card: ")
+                    if m1_p1 in p1_cards:
+                        m1_p2 = input("Player 2 card: ")
+                        if m1_p2 in p2_cards:
+                            print(f"Player 1 chose {m1_p1}! Player 2 chose {m1_p2}!")
+                            if m1_p1 in computer_card:
+                                c1_stats = computer_card_stats[computer_card.index(m1_p1)]
+                                c1_hp = next((d[m1_p1] for d in temp1 if m1_p1 in d), None)
+                                c1_atk = c1_stats[2]
+                                c1_def = c1_stats[3]
+                            if m1_p2 in computer_card:
+                                c2_stats = computer_card_stats[computer_card.index(m1_p2)]
+                                c2_hp = next((d[m1_p2] for d in temp2 if m1_p2 in d), None)
+                                c2_atk = c2_stats[2]
+                                c2_def = c2_stats[3]
+                            if going == 1:
+                                c2_hpn, c2_hp, c2_defed = battle(c1_hp, c1_atk, c1_def, c2_hp, c2_atk, c2_def, 1)
+                                print(f"Player 1's {m1_p1} goes first! It does {(c2_hp - c2_hpn) + c2_defed} damage {m1_p2} blocks {c2_defed} damage! {m1_p2} now has {c2_hpn} HP!")
+                                next(d for d in temp2 if m1_p2 in d)[m1_p2] = c2_hpn
+                                if c2_hp <= 0:
+                                    temp2.remove(next(d for d in temp2 if m1_p2 in d))
+                                    print(temp2)
+                                    if len(temp2) == 0:
                                         c1_w = True
-                                        break
-                                    else:
-                                        going = 2
-                                elif going == 2:
-                                    c1_hpn, c1_hp, c1_defed = battle(c1_hp, c1_atk, c1_def, c2_hp, c2_atk, c2_def, 2)
-                                    print(f"Player 2's {m1_1_r1_p2_c1} goes first! It does {(c1_hp - c1_hpn) + c1_defed} damage {m1_1_r1_p1_c1} blocks {c1_defed} damage! {m1_1_r1_p1_c1} now has {c1_hpn} HP!") 
-                                    c1_hp = c1_hpn
-                                    if c1_hp <= 0:
+                                    break
+                                else:
+                                    going = 2
+                            elif going == 2:
+                                c1_hpn, c1_hp, c1_defed = battle(c1_hp, c1_atk, c1_def, c2_hp, c2_atk, c2_def, 2)
+                                print(f"Player 2's {m1_p2} goes first! It does {(c1_hp - c1_hpn) + c1_defed} damage {m1_p1} blocks {c1_defed} damage! {m1_p1} now has {c1_hpn} HP!") 
+                                next(d for d in temp1 if m1_p1 in d)[m1_p1] = c1_hpn
+                                if c1_hp <= 0:
+                                    temp1.remove(next(d for d in temp1 if m1_p1 in d))
+                                    print(temp1)
+                                    if len(temp1) == 0:
                                         c2_w = True
-                                        break
-                                    else:
-                                        going = 1
-                                time.sleep(1)
-                            else:
-                                print("Invalid card. Try again. ()") 
+                                    break
+                                else:
+                                    going = 1
+                            time.sleep(1)
                         else:
-                            print("Invalid card. Try again.")
-                elif m1_1_r1 == 2:
-                    print("Player 2 goes first!")
-                    going = 1
-                    while True:
-                        p1_w = False
-                        p2_w = False
-                        print(f"Player 1 has: {', '.join(p1_cards)}")
-                        print(f"Player 2 has: {', '.join(p2_cards)}") 
-                        m1_1_r2_p2_c1 = input("Player 2 card: ")
-                        if m1_1_r2_p2_c1 in p2_cards:
-                            m1_1_r2_p1_c1 = input("Player 1 card: ")
-                            if m1_1_r2_p1_c1 in p1_cards:
-                                print(f"Player 1 chose {m1_1_r2_p1_c1}! Player 2 chose {m1_1_r2_p2_c1}!")
-                                if m1_1_r2_p1_c1 in computer_card:
-                                    c1_stats = computer_card_stats[computer_card.index(m1_1_r2_p1_c1)]
-                                    c1_hp = c1_stats[1]
-                                    c1_atk = c1_stats[2]
-                                    c1_def = c1_stats[3]
-                                if m1_1_r2_p2_c1 in computer_card:
-                                    c2_stats = computer_card_stats[computer_card.index(m1_1_r2_p2_c1)]
-                                    c2_hp = c2_stats[1]
-                                    c2_atk = c2_stats[2]
-                                    c2_def = c2_stats[3]
-                                if going == 1:
-                                    c2_hpn, c2_hp, c2_defed = battle(c1_hp, c1_atk, c1_def, c2_hp, c2_atk, c2_def, 1)
-                                    print(f"Player 1's {m1_1_r2_p1_c1} goes first! It does {(c2_hp - c2_hpn) + c2_defed} damage {m1_1_r2_p2_c1} blocks {c2_defed} damage! {m1_1_r2_p2_c1} now has {c2_hpn} HP!")
-                                    c2_hp = c2_hpn
-                                    if c2_hpn <= 0:
-                                        c1_w = True
-                                        break
-                                    else:
-                                        going = 1
-                                elif going == 2:
-                                    c1_hpn, c1_hp, c1_defed = battle(c1_hp, c1_atk, c1_def, c2_hp, c2_atk, c2_def, 2)
-                                    print(f"Player 2's {m1_1_r2_p2_c1} goes first! It does {(c1_hp - c1_hpn) + c1_defed} damage {m1_1_r2_p1_c1} blocks {c1_defed} damage! {m1_1_r2_p1_c1} now has {c1_hpn} HP!")
-                                    c1_hp = c1_hpn
-                                    if c1_hpn <= 0:
-                                        c2_w = True
-                                        break
-                                    else:
-                                        going = 1
-                                time.sleep(1)
-                            else:
-                                print("Invalid card. Try again.") 
-                        else:
-                            print("Invalid card. Try again.")
-                    if c1_w == True:
-                        print("Player 1 wins!")
-                        p1_coins += 50
-                        p2_coins += 15
-                        print("P1: +50 coins \n P2: +15 coins")
-                    elif c2_w == True:
-                        print("Player 2 wins!")
-                        p1_coins += 15
-                        p2_coins += 50
-                        print("P1: +15 coins \n P2: +50 coins")
+                            print("Invalid card. Try again. ()") 
+                    else:
+                        print("Invalid card. Try again.")
+            if c1_w == True:
+                print("Player 1 wins!")
+                p1_coins += 50
+                p2_coins += 15
+                print("P1: +50 coins \n P2: +15 coins")
+            elif c2_w == True:
+                print("Player 2 wins!")
+                p1_coins += 15
+                p2_coins += 50
+                print("P1: +15 coins \n P2: +50 coins")
 
 
 
